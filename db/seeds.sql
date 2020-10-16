0fdcd7258a2df4a6590f8bc2a66f6ce8764921ff
jacksolaro <jacksolaro@gmail.com>
2020-10-16 15:47:14 -0700

    resolved errors. Added burgers_controller logic

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Plain Hambuger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Yummy Burger', false);
+
