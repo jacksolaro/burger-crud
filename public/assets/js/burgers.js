77edf3a06f31d52fc556a931f279982a4ab01caa
jacksolaro <jacksolaro@gmail.com>
2020-10-16 16:06:34 -0700

    added ajax requests in burgers.js

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,41 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".devour").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: {devoured:true}
+      }).then(
+        function() {
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burger-name").val().trim(),
+        devoured: false
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new cat");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
+  });
+  
